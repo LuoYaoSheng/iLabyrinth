c3e87960794680bda1a98e8c275a0afb4041c45d
komashen <sxxdjj@gmail.com>
2013-12-22 21:17:17 +0800

    project

---
+++ proj.android/obj/local/armeabi/objs/cocos2dx_static/__/physics/box2d/CCPhysicsContactInfo_box2d.o.d
@@ -0,0 +1,8 @@
+obj/local/armeabi/objs/cocos2dx_static/__/physics/box2d/CCPhysicsContactInfo_box2d.o: \
+ /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/../physics/box2d/CCPhysicsContactInfo_box2d.cpp \
+ /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/../physics/box2d/CCPhysicsContactInfo_box2d.h \
+ /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/../physics/box2d/../CCPhysicsSetting.h
+
+/Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/../physics/box2d/CCPhysicsContactInfo_box2d.h:
+
+/Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/../physics/box2d/../CCPhysicsSetting.h:
