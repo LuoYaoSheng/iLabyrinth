48b7a0710532ef93b78b1f9afa9efced309f7f0c
komashen <sxxdjj@gmail.com>
2013-12-24 15:51:29 +0800

    deleted .d

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/ccFPSImages.c \
- /Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/ccFPSImages.h
-
-/Volumes/Work/cocos2d-x/cocos2d-x-3.0alpha1/projects/Labyrinth/proj.android/../../../cocos/2d/ccFPSImages.h:
